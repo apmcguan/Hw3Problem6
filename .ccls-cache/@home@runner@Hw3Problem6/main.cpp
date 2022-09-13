227c2cfebdbae4080e6ee775a2a0de7597cfb6f9
apmcguan <111770168+apmcguan@users.noreply.github.com>
2022-09-13 15:32:46 +0000

    new method

--- .ccls-cache/@home@runner@Hw3Problem6/main.cpp
+++ .ccls-cache/@home@runner@Hw3Problem6/main.cpp
@@ -4,22 +4,47 @@ using namespace std;
 
 
 int main() {
-  int H, M, m;
-  cin>> H, M;
-  
-  
+ /* int H, M ;
+  cin >> H >> M ;
+ 
+  H--;
+  M+= 15;
 
-  /*if (m < 0 ){
-    H - 1;
-    M + 60;
-  }
-  if ( H < 0){
-    H = 23;
-    */
-    
+  if (H < 0) {
+    H+= 24;
     
   }
+  if (M>=60) {
+    M -= 60;
+    H++;
+    
+  }
+  cout<< H << " "<< M << endl; 
+  */
+  int H, M ;
+  int AH, AM;
+  cin >> H >> M ;
+
+  if ( M < 45)
+  {
+    AM = M + 60 - 45;
+    AH = H - 1;
+    
+  }
+  else //min greater than equal to 45
+  {
+    AH = H;
+    AM = M - 45;
+  }
+  if (AH == -1)
+  {
+    AH = 23;
+    }
+  cout<<AH<<" "<< AM;
+  
+  }
+
+
  
     
   
-}
--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@ using namespace std;
 
 
 int main() {
-  int H, M ;
+ /* int H, M ;
   cin >> H >> M ;
  
   H--;
@@ -19,16 +19,32 @@ int main() {
     H++;
     
   }
-  
-  
-  
-  
-  
+  cout<< H << " "<< M << endl; 
+  */
+  int H, M ;
+  int AH, AM;
+  cin >> H >> M ;
 
-    
+  if ( M < 45)
+  {
+    AM = M + 60 - 45;
+    AH = H - 1;
     
   }
+  else //min greater than equal to 45
+  {
+    AH = H;
+    AM = M - 45;
+  }
+  if (AH == -1)
+  {
+    AH = 23;
+    }
+  cout<<AH<<" "<< AM;
+  
+  }
+
+
  
     
   
-}
